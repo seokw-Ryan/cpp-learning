4ad5ac8fad1562a3caf99e51d298c8fe4b7c3acd
ggg <seokwoo.chung130@gmail.com>
2024-09-04 17:16:51 +0900

    assignment operator

---
+++ cpp-learning/cpp-assignmentOp/Cube.cpp
@@ -0,0 +1,32 @@
+#include "Cube.h"
+#include <iostream>
+
+namespace prac {
+  Cube::Cube() {
+    length_ = 1;
+    std::cout << "Default constructor invoked! Length=" << length_ << std::endl;
+  }
+
+  Cube::Cube(const Cube & obj) {
+    length_ = obj.length_;
+    std::cout << "Copy constructor invoked!" << std::endl;
+  }
+
+  Cube & Cube::operator=(const Cube & obj) {
+    length_ = obj.length_;
+    std::cout << "Assignment operator invoked!" << std::endl;    
+    return *this;
+  }
+
+  double Cube::getVolume() {
+    return length_ * length_ * length_;
+  }
+
+  double Cube::getSurfaceArea() {
+    return 6 * length_ * length_;
+  }
+
+  void Cube::setLength(double length) {
+    length_ = length;
+  }
+}
---
+++ cpp-learning/cpp-assignmentOp/main.cpp
@@ -0,0 +1,11 @@
+#include "Cube.h"
+using prac::Cube;
+
+int main() {
+  Cube c;
+  Cube myCube;
+
+  myCube = c;
+
+  return 0;
+}
